c01128ff2785af63299210c49f99d0bb8ca42fd1
kayang0000 <18410422493@163.com>
2019-07-12 13:43:13 +0800

    Initial commit

---
+++ src/main/java/com/carry/kqyang/KqyangApplication.java
@@ -0,0 +1,13 @@
+package com.carry.kqyang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KqyangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KqyangApplication.class, args);
+	}
+
+}
